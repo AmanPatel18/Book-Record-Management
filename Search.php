a8e3859ccfaf0329d8516482883b6ae14ca5235d
AmanPatel18 <55862751+AmanPatel18@users.noreply.github.com>
2020-05-26 21:12:14 +0530

    Saerch Operation Added
    Search field is added in View.php file where user can easily Search records from the table of the books stored in a database.

--- Search.php
+++ Search.php
@@ -4,9 +4,10 @@ if(!isset($_SESSION['username']))
 {
     header('location:http://localhost/BRM/Login_Page.php');
 }
+$search=$_POST['search'];
 $con=mysqli_connect('localhost','root','','BRM','3308');
 mysqli_select_db($con,'BRM');
-$q="select * from Book";
+$q="select * from Book where Title='$search'";
 $result=mysqli_query($con,$q);
 $num=mysqli_num_rows($result);
 mysqli_close($con);
@@ -29,18 +30,17 @@ mysqli_close($con);
     <a href="Home.php"><img id="home" src="Home.png"/></a>
     <a href="Login_Page.php"><img id="logout" src="Logout.png"</a>
     <table id="view_table">
-        <tr>
+         <?php
+         if($num==1)
+         {
+             $row=mysqli_fetch_array($result);
+         ?>
+         <tr>
         <th>Book ID</th>
         <th>Title</th>
         <th>Author</th>
         <th>Price</th>
         </tr>
-        <tr>
-        <?php
-        for($i=1;$i<=$num;$i++)
-        {
-           $row=mysqli_fetch_array($result);
-        ?>
         <tr>
         <td><?php echo $row['BookId']?></td>
         <td><?php echo $row['Title']?></td>
@@ -48,9 +48,12 @@ mysqli_close($con);
         <td><?php echo $row['Price']?></td>
         </tr>
         <?php
-        }
+         }
+         else
+         {
+             header("location:http://localhost/brm/View.php");
+         }
         ?>
-        </tr>
     </table>
 </body>
 </html>
